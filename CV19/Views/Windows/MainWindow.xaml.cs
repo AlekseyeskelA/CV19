cd40c0de2db4912598f4114727f4d6a3f5ebeee1
Алексей Бондарь <1.blin.com@gmail.com>
2022-04-04 23:56:09 +0300

    PropertyChanged

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ namespace CV19
 { 
     public partial class MainWindow : Window
     {
+        // В идеале здесь не должно быть никаких обрадотчиков событий из разметки. Всё должно быть в том виде, в каком было создано Visual Studio.
         public MainWindow()
         {
             InitializeComponent();
