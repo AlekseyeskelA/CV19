2f49f4476b340097acf15705be7302c41ef88552
Алексей Бондарь <1.blin.com@gmail.com>
2022-08-07 21:52:27 +0300

    Тестовая предметная область

---
+++ CV19/Models/Decanat/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CV19.Models.Decanat
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Surename { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+        public double Rating { get; set; }
+    }
+
+    internal class Group
+    {
+        public string Name { get; set; }
+
+        // Свойство Students зададим как ICollection, чтобы можно было получить свободу выбора того, какую коллекцию сюда добавлять (List, Array, Hesh-таблица, Observablecollection и т.д.) (Queue нельзя добавить):
+        public ICollection<Student> Students { get; set; }
+    }
+}
