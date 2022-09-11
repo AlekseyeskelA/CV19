d4100350b28369f650b0709b9c41566670241915
Алексей Бондарь <1.blin.com@gmail.com>
2022-09-11 16:32:30 +0300

    Применение иерархического шаблона отображения иерархических данных в визуальном элементе TreeView

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -232,6 +232,19 @@ namespace CV19.ViewModels
                 });
 
 
+        public DirectoryViewModel DiskRootDir { get; } = new DirectoryViewModel("c:\\");
+
+        #region SelectedDirectory: DirectoryViewModel - Выбранная директория
+
+        /// <summary>Выбранная директория</summary>
+        private DirectoryViewModel _SelectedDirectory;
+
+        /// <summary>Выбранная директория</summary>
+        public DirectoryViewModel SelectedDirectory { get => _SelectedDirectory; set => Set(ref _SelectedDirectory, value); }
+
+        #endregion
+
+
         /*------------------------------------------------------------------------------------------------------------------------------------------*/
 
         #region Команды
