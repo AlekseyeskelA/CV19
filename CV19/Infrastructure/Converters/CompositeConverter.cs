75ca1fe6d359068240eb347a7bc8e0173bb8750e
Алексей Бондарь <1.blin.com@gmail.com>
2022-10-31 11:59:29 +0300

    Больше конвертеров хороших и разных! Комплексный конвертер

---
+++ CV19/Infrastructure/Converters/CompositeConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    /* Данный конвертер можно применять для композиции, т.е., когда нужно выполнить не одно преобразование, а несколько. С помощью этого конвертера можно комбинировать
+    любое количество других конвертеров между собой */
+    internal class CompositeConverter : Converter
+    {
+        // У данного конвертера будет два свойства-конвертера, которые мы поочереди будем вызывать:
+        public IValueConverter First { get; set; }
+        public IValueConverter Second { get; set; }
+
+        public override object Convert(object v, Type t, object p, CultureInfo c)
+        {
+            // Вызываем первый конвертер, подразумевая возможность, что его может не быть "?", и вызываем метод Convert, передавая ему параметры v, t, p, c, и если его нет "?"",
+            // то берём значение v, которое было:
+            var result1 = First?.Convert(v, t, p, c) ?? v;
+            // После этого вызываем второй конвертер:
+            var result2 = Second?.Convert(result1, t, p, c) ?? result1;
+
+            return result2;
+        }
+
+        public override object ConvertBack(object v, Type t, object p, CultureInfo c)
+        {
+            var result2 = Second?.ConvertBack(v, t, p, c) ?? v;
+            var result1 = First?.ConvertBack(result2, t, p, c) ?? result2;
+
+            return result1;
+        }
+    }
+}
--- CV19/Infrastructure/Converters/Linear.cs
+++ CV19/Infrastructure/Converters/Linear.cs
@@ -1,17 +1,40 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace CV19.Infrastructure.Converters
 {
-    /// <summary>
-    /// Реализация линейнгго преобразования f(x) = k*x + b
-    /// </summary>
+    /// <summary>Реализация линейнгго преобразования f(x) = k*x + b</summary>
+
+    [ValueConversion(typeof(double), typeof(double))]
+
     internal class Linear : Converter
     {
-        // Зададим значение коэффициентов по умолчанию:
-        public double K { get; set; } = 1;
-        public double B { get; set; } = 0;
+        // Свойства:
+        // Зададим значение коэффициентов по умолчанию.
+        // Укажем системе, что эти свойства связаны с аргументами конструкторов K и B:
 
+        [ConstructorArgument("K")]
+        public double K { get; set; } = 1;
+
+        [ConstructorArgument("B")]
+        public double B { get; set; }
+
+
+
+        // Конструкторы (установим каскадным образом):
+        /*Данные конструкторы просто так сейчас вызвать невозможно при формированиии объекта App.xaml в разметке.*/
+        public Linear() { }         // Этот конструктор ничего делать не будет, позволив нам создавать объекты без параметров.
+
+        public Linear(double K) => this.K = K;
+
+        public Linear(double K, double B) : this(K) => this.B = B;
+
+
+
+        // Методы:
         public override object Convert(object v, Type t, object p, CultureInfo c)
         {
             if (v is null) return null;
@@ -23,6 +46,8 @@ namespace CV19.Infrastructure.Converters
         public override object ConvertBack(object v, Type t, object p, CultureInfo c)
         {
             if (v is null) return null;
+
+            //var type = TypeDescriptor.AddAttributes(t);
 
             var y = System.Convert.ToDouble(v, c);
 
