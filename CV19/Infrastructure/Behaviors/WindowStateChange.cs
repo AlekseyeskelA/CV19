3d3b833ca3927de38b87103bbc8a563816e22774
Алексей Бондарь <1.blin.com@gmail.com>
2023-12-27 12:18:57 +0300

    Методы-расширения для DependencyObject, позволяющие нацти его  корень в дереве (логическом или визуальном)

---
+++ CV19/Infrastructure/Behaviors/WindowStateChange.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace CV19.Infrastructure.Behaviors
+{
+    class WindowStateChange : Behavior<Button>
+    {
+    }
+}
---
+++ CV19/Infrastructure/Extentions/DependencyObjectExtentions.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Media;
+
+namespace System.Windows
+{
+    //Класс должен быть статическим статическим. И поместим его в простанство имён using System.Windows (добавим в заголовке):
+    static class DependencyObjectExtentions
+    {
+        //У нас будет два метода, которые позволяют для любого визуального объекта DependencyObject найти его корневой элемент:
+        public static DependencyObject FindVisualRoot (this DependencyObject obj)
+        {
+            do
+            {
+                var parent = VisualTreeHelper.GetParent(obj);
+                if (parent is null) return obj;
+                obj = parent;
+            }
+            while (true);
+        }
+
+        public static DependencyObject FindLogicalRoot(this DependencyObject obj)
+        {
+            do
+            {
+                var parent = LogicalTreeHelper.GetParent(obj);
+                if (parent is null) return obj;
+                obj = parent;
+            }
+            while (true);
+        }
+    }
+}
