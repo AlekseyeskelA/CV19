295569cddd31aa904cc08aba05bfa1b7801c7725
Алексей Бондарь <1.blin.com@gmail.com>
2024-01-24 13:11:43 +0300

    Методы-расширения для поиска элемента среди родительских в визуальном или логическом деревьях.

---
+++ CV19/Infrastructure/Behaviors/WindowsTitleBarBehavior.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+
+namespace CV19.Infrastructure.Behaviors
+{
+    /* : Behavior<UIElement> - наделим этот класс поведением для любого элемента, чтобы его можно было привязать к любому визуальному элементу */
+    class WindowsTitleBarBehavior : Behavior<UIElement>
+    {
+        private Window _Window;
+
+        /* Присосединение: */
+        protected override void OnAttached()
+        {
+            _Window = AssociatedObject as Window ?? AssociatedObject.Fi;
+        }
+
+        /* Отсосединение: */
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+        }
+    }
+}
--- CV19/Infrastructure/Extentions/DependencyObjectExtentions.cs
+++ CV19/Infrastructure/Extentions/DependencyObjectExtentions.cs
@@ -27,5 +27,38 @@ namespace System.Windows
             }
             while (true);
         }
+
+        /* Добавим третий метод (шаблонный) для поиска родительского элемента нужного типа. Будем указывать тип элемента, который мы ищем, вверх по дереву.
+         * Тип ограничен тем, что это будет любой объект-наследник DependencyObject: */
+        public static T FindVisualParent<T>(this DependencyObject obj) 
+            where T : DependencyObject
+        {
+            if (obj is null) return null;
+            var target = obj;
+            /* Далее в цикле с помощью класса VisualTreeHelper вызываем метод GetParent, т.е. поднимаемся вверх до тех пор пока target не равен null (т.е. пустая ссылка), либо target
+               не является указанным типом:*/
+            do
+            {
+                target = VisualTreeHelper.GetParent(target);
+            }
+            while (target != null && !(target is T));
+            return target as T;
+        }
+
+        /* Сделаем копию метода для логического дерева: */
+        public static T FindLagicalParent<T>(this DependencyObject obj) 
+            where T : DependencyObject
+        {
+            if (obj is null) return null;
+            var target = obj;
+            /* Далее в цикле с помощью класса VisualTreeHelper вызываем метод GetParent, т.е. поднимаемся вверх до тех пор пока target не равен null (т.е. пустая ссылка), либо target
+               не является указанным типом:*/
+            do
+            {
+                target = LogicalTreeHelper.GetParent(target);
+            }
+            while (target != null && !(target is T));
+            return target as T;
+        }
     }
 }
