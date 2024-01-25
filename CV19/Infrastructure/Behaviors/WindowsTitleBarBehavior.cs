c72d70984656fd91c49070598cc389255e6bc0ce
Алексей Бондарь <1.blin.com@gmail.com>
2024-01-25 11:35:37 +0300

    Обеспечиваем возможность перетаскивания окна за заголовок.

--- CV19/Infrastructure/Behaviors/WindowsTitleBarBehavior.cs
+++ CV19/Infrastructure/Behaviors/WindowsTitleBarBehavior.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xaml.Behaviors;
+using System;
 using System.Windows;
+using System.Windows.Input;
 
 namespace CV19.Infrastructure.Behaviors
 {
@@ -11,13 +13,26 @@ namespace CV19.Infrastructure.Behaviors
         /* Присосединение: */
         protected override void OnAttached()
         {
-            _Window = AssociatedObject as Window ?? AssociatedObject.Fi;
+            /* Рассматриваем текущий метод как окно. Если это не окно. то  */
+            _Window = AssociatedObject as Window ?? AssociatedObject.FindLagicalParent<Window>();
+
+            /* Реализуем перемещение окна. для этого нам нужно добавить обработчики события: */
+            AssociatedObject.MouseLeftButtonDown += OnMouseDown;
         }
 
         /* Отсосединение: */
         protected override void OnDetaching()
         {
-            base.OnDetaching();
+            /* Отпишемся от события, если оно нам больше не нужно. Зааодно обнулим ссылку на окно: */
+            AssociatedObject.MouseLeftButtonDown -= OnMouseDown;
+            _Window = null;
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount > 1) return; // Это условие нужно для того, чтобы обработать развёртывание окна двойным кликом по заголовку.
+            if (!(AssociatedObject.FindVisualRoot() is Window window)) return;
+            window.DragMove();
         }
     }
 }
